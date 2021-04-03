85da371d1dfcf26e5d7e7a1f506624d37a259979
itxingqing <itxingqing@users.noreply.github.com>
2021-04-03 08:27:29 +0800

    update the student code

--- src/main/java/pool/Student.java
+++ src/main/java/pool/Student.java
@@ -24,4 +24,8 @@ public class Student {
                 ", count=" + count +
                 '}';
     }
+
+    public static void main(String[] args) {
+        System.out.println(">>>>>>");
+    }
 }
